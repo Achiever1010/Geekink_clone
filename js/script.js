6a7fc3ba0b95186c21c46363315f1c5fbeeb9725
Achiever1010 <usmanadamuusman94@gmail.com>
2025-11-12 12:27:12 +0100

    feat

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const ready={
+    transform:'Ready to Transform Your Team',
+    join:'Join 2 organizations already using GeekInk to upskill thier workforce.',
+    get:' Get a personalized demo and see the difference.',
+    request:'Request Demo',
+    quote:'Get Quote',
+    contact:'Contact Instantly via WhatsApp no forms no waiting',
+};
+document.getElementById('team-1').innerText=ready.transform;
+document.getElementById('team-2').innerText=ready.join;
+document.getElementById('team-4').innerText=ready.request;
+document.getElementById('team-5').innerText=ready.quote;
+document.getElementById('team-6').innerText=ready.contact;
